5fc6a23544618ac872dcf7124083a25e49f923a6
Pavel Sarwar <pavelsarwar@users.noreply.github.com>
2018-04-02 18:04:12 +0800

    new

---
+++ js/script.js
@@ -0,0 +1,73 @@
+$( document ).ready(function() {
+	
+  		$("#btn_pause").hide();
+		
+
+});
+
+
+ var player = new Audio();
+ player.autobuffer = true;
+
+ var channel = ['http://144.217.129.213:8520/;stream'];
+ var current_channel = 0;
+ var channel_img = ["images/radiotoday.png"]
+
+$("#btn_play").click(function(){
+
+			$("#btn_play").hide();
+			$("#btn_pause").show();
+			play(channel[current_channel%5]);
+			console.log("calling")
+});
+
+
+$("#btn_pause").click(function(){
+
+			$("#btn_pause").hide();
+			$("#btn_play").show();
+			
+			pause();
+			// console.log("calling");
+});
+
+
+$("#btn_next").click(function(){
+
+		player.pause();
+		$("#logo").attr("src", channel_img[(current_channel+1)%5]);
+		$("#btn_play").show();
+		$("#btn_pause").hide();	
+		current_channel += 1;
+			
+});
+
+
+
+$("#btn_prev").click(function(){
+
+		if(current_channel != 0)
+		{
+			player.pause();
+		$("#logo").attr("src", channel_img[(current_channel-1)%5]);
+		$("#btn_play").show();
+		$("#btn_pause").hide();	
+		current_channel -= 1;
+			
+		}
+		
+			
+});
+function play(url){
+
+	 
+	  player.src = url;
+	  player.volume = 1;
+	  player.play();
+	  console.log("playing");
+}
+
+function pause(){
+
+	player.pause();
+}
